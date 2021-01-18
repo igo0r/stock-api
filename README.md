be6765b4d96ed2cb7bb596fb0f94391ecffcf719
igorlantushenko <igor.lantushenko@gmail.com>
2021-01-18 11:14:40 +0200

    Init project

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # stock-api
+
+Start api server:
+```node index.js```
+We are making requests to the `https://finance.yahoo.com/quote/${stock}/`
+
+Init client:
+https://github.com/igo0r/stock-tracker
---
+++ index.js
@@ -0,0 +1,65 @@
+//Manual https://levelup.gitconnected.com/stocks-api-tutorial-with-javascript-40f24320128c
+
+
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const fetch = require("node-fetch");
+const axios = require("axios");
+
+const cors = require("cors");
+app.use(cors());
+app.options('*', cors());
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+require("dotenv").config();
+
+app.get("/stock", cors(), async (req, res) => {
+    const body = JSON.parse(JSON.stringify(req.body));
+    const {ticker, type} = body;
+    console.log("stocks-api.js 14 | body", body.ticker);
+    const request = await fetch(
+        `https://www.alphavantage.co/query?function=${timePeriod(
+            type
+        )}&symbol=${ticker}&apikey=${process.env.ALPHA_VANTAGE_API_KEY}`
+    );
+    const data = await request.json();
+    res.json({data: data});
+});
+
+app.post("/stocks", async (req, res) => {
+    let body = JSON.parse(JSON.stringify(req.body));
+    let stocksPromises = await body.stocks.map(async stock => {
+        console.log(stock);
+        let request = await axios.get(
+            `https://finance.yahoo.com/quote/${stock}/`
+        );
+        let price = request.data
+            .split(`"${stock}":{"sourceInterval"`)[1]
+            .split('regularMarketPrice')[1]
+            .split('fmt":"')[1]
+            .split('"')[0];
+        return {[stock]: price}
+    });
+
+    Promise.all(stocksPromises)
+        .then(values => {
+            let response = {};
+            for (let value of values) {
+                let stockName = Object.keys(value)[0];
+                response[stockName] = value[stockName];
+            }
+            console.log("stocks-api.js 40 | values", response);
+            res.json({data: response, status: "done"});
+        })
+        .catch(error => {
+            console.log("stocks-api.js 47 | error", error);
+            res.json({error: error});
+        });
+});
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log("index.js 6 | server started...");
+});
